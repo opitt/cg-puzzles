446c72eac08285fc81f4f9751d0ea972bc7711d7
Karsten Stiegler <opitt@web.de>
2021-06-29 20:51:13 +0200

    fixed lower part of the diamond

--- indianalphabet.py
+++ indianalphabet.py
@@ -7,4 +7,4 @@ for i in range(1, n + 1):
     s = "-".join(line[:i]).rjust(n * 2 - 1, "-")
     out.append(s[:-1] + s[::-1])
 print("\n".join(out))
-print("\n".join(out[1::-1]))
+print("\n".join(out[-2::-1])) # list(reversed(out))[1:]
